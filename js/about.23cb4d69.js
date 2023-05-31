87a9cfa7c8e330980ec410372ef072455793cd08
Sasha <106412494+AalexSanders@users.noreply.github.com>
2023-05-31 22:17:56 +0500

    удалить

--- js/about.23cb4d69.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkshop"]=self["webpackChunkshop"]||[]).push([[443],{242:function(n,s,u){u.r(s),u.d(s,{default:function(){return f}});var t=u(252);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),r=[e];function c(n,s){return(0,t.wg)(),(0,t.iD)("div",a,r)}var o=u(744);const i={},h=(0,o.Z)(i,[["render",c]]);var f=h}}]);
-//# sourceMappingURL=about.23cb4d69.js.map
